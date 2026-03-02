7d20fde2ee36f405fcc19fc60ab187264dd4fe79
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2026-03-02 17:20:36 -0500

    Object selector

---
+++ Assets/Features/ObjectSelector/Scripts/ObjectSelector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class ObjectSelector : MonoBehaviour
+{
+    private Camera _camera;
+    
+    [SerializeField] private LayerMask _selectableLayerMask;
+    [SerializeField] private float _raycastMaxDistance = 100f;
+    [field: SerializeField, ReadOnly] public SelectableObject CurrentlySelectedObject { get; private set; }
+    [SerializeField, Required] private Material _selectedMaterial;
+
+    private void Awake()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        Ray mouseRay = _camera.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(mouseRay, out RaycastHit hit, _raycastMaxDistance, _selectableLayerMask);
+        
+        SelectableObject hitSelectableObject = hit.collider == null ? null : hit.collider.GetComponent<SelectableObject>();
+        if (hitSelectableObject != CurrentlySelectedObject)
+        {
+            // deselect prev
+            if(CurrentlySelectedObject != null)
+                CurrentlySelectedObject.ResetMaterials();
+            
+            CurrentlySelectedObject = hitSelectableObject;
+            if(CurrentlySelectedObject != null)
+                CurrentlySelectedObject.ChangeMaterials(_selectedMaterial);
+        }
+    }
+}
---
+++ Assets/Features/ObjectSelector/Scripts/SelectableObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SelectableObject : MonoBehaviour
+{
+    private Dictionary<Renderer, Material[]> _originalMaterials = new Dictionary<Renderer, Material[]>();
+    
+    private void Awake()
+    {
+        GetOriginalMaterials();
+    }
+
+    private void GetOriginalMaterials()
+    {
+        foreach (var r in GetComponentsInChildren<Renderer>())
+        {
+            _originalMaterials.Add(GetComponent<Renderer>(), GetComponent<Renderer>().sharedMaterials);
+        }
+    }
+
+    public void ResetMaterials()
+    {
+        foreach (var kvp in _originalMaterials)
+        {
+            kvp.Key.SetMaterials(kvp.Value.ToList());
+        }
+    }
+    
+    public void ChangeMaterials(Material newMaterial)
+    {
+        foreach (var kvp in _originalMaterials)
+        {
+            kvp.Key.SetMaterials(new List<Material>(){newMaterial});
+        }
+    }
+}
