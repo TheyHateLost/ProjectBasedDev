bc9ae6e307dcdd79a9efb05fc7789f6812f017ea
Michael <thelostkid224@gmail.com>
2026-04-12 17:51:35 -0400

    BTU calc

---
+++ Assets/Features/BTU/BTUCalculator.cs
@@ -0,0 +1,31 @@
+using UnityEngine;  
+
+public class BTUCalculator : MonoBehaviour
+{
+   public enum RoomType {Bathroom}
+   
+   [Header("Room Type")]
+   public RoomType roomType;
+   public float width; 
+   public float length;
+   public float height;
+
+private float GetMultiplier(RoomType type)
+{
+    switch (type)
+    {
+        case RoomType.Bathroom: return 148.5f;
+        default: return 0;
+    }
+
+}
+
+    public float CalculateBTU()
+    {
+        float volume = width * length * height;
+        float multiplier = GetMultiplier(roomType);
+        float btu = volume * multiplier;
+        return btu;
+    }
+
+}
